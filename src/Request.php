0a1a5b7d7c305b706c028573feac4dfa4e313b15
Oliver Etchebarne <yo@drmad.org>
2021-08-26 15:35:44 -0500

    Fix return type for method checking

--- src/Request.php
+++ src/Request.php
@@ -17,7 +17,7 @@ class Request extends Psr\ServerRequest
         $parsed_url = parse_url($_SERVER['REQUEST_URI']);
 
         $uri = (new Psr\Uri())
-            ->withScheme($_SERVER['REQUEST_SCHEME'] ?? 
+            ->withScheme($_SERVER['REQUEST_SCHEME'] ??
                 (($_SERVER['HTTPS'] ?? false) ? 'https' : 'http')
             )
             ->withHost($_SERVER['HTTP_HOST'])
@@ -39,42 +39,42 @@ class Request extends Psr\ServerRequest
         return $server_request;
     }
 
-    /** 
+    /**
      * Returns true if the method is GET
      */
-    public function isGet(): boolean
+    public function isGet(): bool
     {
         return strtolower($this->getMethod()) == 'get';
     }
 
-    /** 
+    /**
      * Returns true if the method is POST
      */
-    public function isPost(): boolean
+    public function isPost(): bool
     {
         return strtolower($this->getMethod()) == 'post';
     }
 
-    /** 
+    /**
      * Returns true if the method is PUT
      */
-    public function isPut(): boolean
+    public function isPut(): bool
     {
         return strtolower($this->getMethod()) == 'put';
     }
 
-    /** 
+    /**
      * Returns true if the method is DELETE
      */
-    public function isDelete(): boolean
+    public function isDelete(): bool
     {
         return strtolower($this->getMethod()) == 'delete';
     }
 
-    /** 
+    /**
      * Returns true if the method is PATCH
      */
-    public function isPatch(): boolean
+    public function isPatch(): bool
     {
         return strtolower($this->getMethod()) == 'patch';
     }
