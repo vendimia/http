545d7d78beb2dc06c3e952a3d466008080d88cab
Oliver Etchebarne <yo@drmad.org>
2021-09-09 12:55:53 -0500

    Add 'redirect' static method for create a HTTP 302 response

--- src/Response.php
+++ src/Response.php
@@ -1,7 +1,7 @@
 <?php
 namespace Vendimia\Http;
 
-/** 
+/**
  * HTTP Response to the client
  */
 class Response extends Psr\Response
@@ -10,13 +10,24 @@ class Response extends Psr\Response
     {
         $body = new Psr\Stream('php://temp', 'w');
         $body->write($string);
-        
+
         return (new self)
             ->withBody($body)
         ;
     }
 
-    /** 
+    /**
+     * Creates a response for redirect to another url, using HTTP 302.
+     */
+    public static function redirect($url): self
+    {
+        return (new self)
+            ->withStatus(302)
+            ->withHeader('Location', $url)
+        ;
+    }
+
+    /**
      * Creates an actual HTTP response and sends it to the client.
      */
     public function send()
@@ -34,7 +45,7 @@ class Response extends Psr\Response
                 header("{$name}: {$value}");
             }
         }
-        
+
         // Enviamos la data
         $this->getBody()->rewind();
         echo $this->getBody()->getContents();
