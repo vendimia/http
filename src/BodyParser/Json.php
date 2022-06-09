0e854c8afd9cf950b06f5fad38ae14c1fc80e370
Oliver Etchebarne <yo@drmad.org>
2022-06-09 11:08:47 -0500

    Add try/catch at JSON parsing

--- src/BodyParser/Json.php
+++ src/BodyParser/Json.php
@@ -1,6 +1,10 @@
 <?php
 namespace Vendimia\Http\BodyParser;
 
+use Exception;
+use JsonException;
+use Vendimia\Exception\VendimiaException;
+
 /**
  * Parses a JSON body to an array.
  *
@@ -15,6 +19,23 @@ class Json implements BodyParserInterface
 
     public static function parse($source): array
     {
-        return json_decode($source, associative: true);
+        try {
+            $result = json_decode(
+                $source,
+                associative: true,
+                flags: JSON_THROW_ON_ERROR
+            );
+        } catch (JsonException $e) {
+            // Lo rethrowamos
+            if (class_exists(VendimiaException::class)) {
+                throw new VendimiaException("Error parsing JSON body: " . $e->getMessage(),
+                    original_body: $source,
+                );
+            } else {
+                throw new Exception("Error parsing JSON body: " . $e->getMessage());
+            }
+        }
+
+        return $result;
     }
 }
