338d9a717c3dd1460d7e34ed029d6122ec704aeb
Oliver Etchebarne <yo@drmad.org>
2024-01-15 09:55:57 -0500

    Added default reason phrases

--- src/Psr/Response.php
+++ src/Psr/Response.php
@@ -6,12 +6,57 @@ use Psr\Http\Message\UriInterface;
 use InvalidArgumentException;
 
 /**
- * Vendimia PSR-7 ResponseInterface implementation
+ * Vendimia PSR-7 Response interface implementation
  *
  * @author Oliver Etchebarne <yo@drmad.org>
  */
 class Response extends Message implements ResponseInterface
 {
+    /** Reason phrases from https://datatracker.ietf.org/doc/html/rfc7231 */
+    private const REASON_PHRASE = [
+        100 => "Continue",
+        101 => "Switching Protocols",
+        200 => "OK",
+        201 => "Created",
+        202 => "Accepted",
+        203 => "Non-Authoritative Information",
+        204 => "No Content",
+        205 => "Reset Content",
+        206 => "Partial Content",
+        300 => "Multiple Choices",
+        301 => "Moved Permanently",
+        302 => "Found",
+        303 => "See Other",
+        304 => "Not Modified",
+        305 => "Use Proxy",
+        307 => "Temporary Redirect",
+        400 => "Bad Request",
+        401 => "Unauthorized",
+        402 => "Payment Required",
+        403 => "Forbidden",
+        404 => "Not Found",
+        405 => "Method Not Allowed",
+        406 => "Not Acceptable",
+        407 => "Proxy Authentication Required",
+        408 => "Request Timeout",
+        409 => "Conflict",
+        410 => "Gone",
+        411 => "Length Required",
+        412 => "Precondition Failed",
+        413 => "Payload Too Large",
+        414 => "URI Too Long",
+        415 => "Unsupported Media Type",
+        416 => "Range Not Satisfiable",
+        417 => "Expectation Failed",
+        426 => "Upgrade Required",
+        500 => "Internal Server Error",
+        501 => "Not Implemented",
+        502 => "Bad Gateway",
+        503 => "Service Unavailable",
+        504 => "Gateway Timeout",
+        505 => "HTTP Version Not Supported",
+    ];
+
     private $code = 200;
     private $reason = 'OK';
 
@@ -23,6 +68,14 @@ class Response extends Message implements ResponseInterface
     public function withStatus($code, $reasonPhrase = ''): self
     {
         $response = clone $this;
+
+        if (!$reasonPhrase) {
+            if (!key_exists($code, self::REASON_PHRASE)) {
+                throw new InvalidArgumentException("Invalid status code");
+            }
+
+            $reasonPhrase = self::REASON_PHRASE[$code];
+        }
 
         $response->code = $code;
         $response->reason = $reasonPhrase;
